26ac12dd42176eb4380980c5054089dc977f599d
Edwin Shibu Mathew <iedwini777@gmail.com>
2025-09-20 10:07:44 +0100

    Completed task 1 and task 2

---
+++ src/main/java/com/jpmc/midascore/service/TransactionConsumer.java
@@ -0,0 +1,28 @@
+package com.jpmc.midascore.service;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionConsumer.class);
+    private int transactionCount = 0;
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "transaction-consumer-group")
+    public void consumeTransaction(Transaction transaction) {
+        transactionCount++;
+        logger.info("Received transaction #{}: {}", transactionCount, transaction);
+        
+        // Log the amount for debugging purposes - especially the first 4
+        if (transactionCount <= 4) {
+            logger.info("*** DEBUG: Transaction #{} amount is: {} ***", transactionCount, transaction.getAmount());
+        }
+        
+        // For debugging - set a breakpoint here to examine transaction details
+        System.out.println("Processing transaction: " + transaction);
+    }
+}
